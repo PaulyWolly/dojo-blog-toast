66fc30779f50ea6f96b50fa53618a6f5a8117279
Paul Welby <pwelby@gmail.com>
2022-01-05 14:24:04 -0800

    created useFetch AbortController to stop fetch causing error

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import './App.css';
 import ToastComponent from './ToastComponent';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Create from './components/NewBlog';
+
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="row">
-        <div className="col-lg-1"></div>
-        <div className="col-lg-10">
-          <div className="content">
-            <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="row">
+          <div className="col-lg-1"></div>
+          <div className="col-lg-10">
+            <div className="content">
+              <Routes>
+                <Route exact path="/" element={<Home />}></Route>
+                <Route exact path="/Create" element={<Create />}></Route>
+              </Routes>
+            </div>
           </div>
+          <div className="col-lg-1"></div>
         </div>
-        <div className="col-lg-1"></div>
-      </div>
 
-      <ToastComponent />
-    </div>
+        <ToastComponent />
+      </div>
+    </Router>
   );
 }
 
