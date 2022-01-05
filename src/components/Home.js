698a9b6245ca675fb6207856f7ca13c1c224560c
Paul Welby <pwelby@gmail.com>
2022-01-05 09:53:35 -0800

    added 'loading message' added Error catching and throwing to screen, Added JSON server

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,47 @@
-import { useState } from "react"
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react"
+import ToastComponent from "../ToastComponent";
 import BlogList from "./BlogList";
 /* eslint-disable no-unused-vars */
 
 const Home = () => {
 
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'Yoshi', id: 1},
-    { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'Mario', id: 2},
-    //{ title: 'Web Dev top tips', body: 'lorem ipsum...', author: 'Paul', id: 3},
-    { title: 'Welcome Party2', body: 'lorem ipsum...', author: 'Mario', id: 4},
-    // { title: 'Mario\'s Welcome Party!', body: 'lorem ipsum...', author: 'Mario', id: 5},
-    // { title: 'Do the Dance!!', body: 'lorem ipsum...', author: 'Mario', id: 6},
-  ])
+  // useState = React HOOK (1) of many
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null)
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs);
 
-  }
+  useEffect(() => {
+    fetch ('http://localhost:8000/blogs')
+      .then(res => {
+
+        if (!res.ok) {
+          throw Error('Could not fetch the data for that resource.')
+        }
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null)
+      })
+      .catch(err => {
+        //console.log(err.message)
+        setError(err.message)
+        setIsPending(false)
+      })
+
+  }, [])
 
   return (
 
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs" />
+      {error && <div>{ error }</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs" handleDelete={handleDelete} />} */}
+
     </div>
 
   );
