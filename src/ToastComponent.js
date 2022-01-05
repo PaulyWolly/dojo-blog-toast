836dcf10b010d5705171713229ae9e6f27a69315
Paul Welby <pwelby@gmail.com>
2022-01-05 07:04:28 -0800

    Added fixed footer. Added create new BlogList. Using 'props'. scrolling

--- src/ToastComponent.js
+++ src/ToastComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
 import Toast from 'react-bootstrap/Toast';
 
@@ -23,36 +24,40 @@ export default function ToastComponent() {
                 duration={5000}
                 classNames={['info']}  // 'success', 'info', 'warning', 'error'
             /> */}
-            <button onClick={toggleShowA} className="btn-sm btn-primary mb-2">
+            {/* <button onClick={toggleShowA} className="btn-sm btn-primary mb-2">
                 Toggle Toast <strong>with</strong> Animation
-            </button>
+            </button> */}
 
-            <div className="position">
-                <ToastContainer  position='middle-center' >
-                    <Toast
-                        show={showA}
-                        onClose={toggleShowA}
-                        delay={5000}
-                        autohide={true}
-                        bg={'primary'} //'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'dark' | 'light'
+            <footer className="footer">
+                <div className="container">
+                    <a onClick={toggleShowA}><span className="iconify" data-icon="la:infinity"></span></a>
+                </div>
+            </footer>
 
-                    >
-                    <Toast.Header>
-                        {/* <img
-                        src="holder.js/20x20?text=%20"
-                        className="rounded me-2"
-                        alt=""
-                        /> */}
-                        <span className="loggedState iconify" data-icon="tabler:brand-react-native"></span>
-                        <strong className="me-auto">&nbsp;&nbsp;&nbsp;Bootstrap</strong>
-                        <small>11 mins ago</small>
-                    </Toast.Header>
-                    <Toast.Body className='toast-body'>
-                        Woohoo, you're reading this text in a Toast!
-                    </Toast.Body>
-                    </Toast>
-                </ToastContainer>
-            </div>
+            <ToastContainer position='middle-center' >
+                <Toast
+                    show={showA}
+                    onClose={toggleShowA}
+                    delay={5000}
+                    autohide={true}
+                    bg={'primary'} //'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'dark' | 'light'
+                >
+                <Toast.Header>
+                    {/* <img
+                    src="holder.js/20x20?text=%20"
+                    className="rounded me-2"
+                    alt=""
+                    /> */}
+                    <span className="loggedState iconify" data-icon="tabler:brand-react-native"></span>
+                    <strong className="me-auto">&nbsp;&nbsp;&nbsp;Bootstrap</strong>
+                    <small>11 mins ago</small>
+                </Toast.Header>
+                <Toast.Body className='toast-body'>
+                    Woohoo, you're reading this text in a Toast!
+                </Toast.Body>
+                </Toast>
+            </ToastContainer>
+
         </div>
     );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,29 @@
 import { useState } from "react"
+import BlogList from "./BlogList";
 /* eslint-disable no-unused-vars */
+
 const Home = () => {
 
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25)
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'Yoshi', id: 1},
+    { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'Mario', id: 2},
+    //{ title: 'Web Dev top tips', body: 'lorem ipsum...', author: 'Paul', id: 3},
+    { title: 'Welcome Party2', body: 'lorem ipsum...', author: 'Mario', id: 4},
+    // { title: 'Mario\'s Welcome Party!', body: 'lorem ipsum...', author: 'Mario', id: 5},
+    // { title: 'Do the Dance!!', body: 'lorem ipsum...', author: 'Mario', id: 6},
+  ])
 
-  const handleClick = () => {
-  setName('LUIGI');
-  setAge(30);
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id)
+    setBlogs(newBlogs);
 
   }
 
   return (
 
     <div className="home">
-      <h2>Home page</h2>
-      <p>{name} is { age } years old.</p>
-      <button onClick={handleClick}>Click Me</button>
-
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs" />
     </div>
 
   );
